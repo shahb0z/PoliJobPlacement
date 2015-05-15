1a35e37de61bf6c03ce67ac9412a068c1be6084b
shahboz <shahbozbek.as@gmail.com>
2015-05-15 15:33:43 +0200

    Data Model Done v1.0

--- app/src/main/java/it/polito/mobile/polijobplacement/Data/Company.java
+++ app/src/main/java/it/polito/mobile/polijobplacement/Data/Company.java
@@ -34,18 +34,6 @@ public class Company extends App_User {
         add(JobApplication.JOB_OFFERS, item);
     }
 
-    //TODo   add by kukush set Company name
-    public String getName() {
-        return getString(JobApplication.NAME);
-    }
-
-    public void setName(String name) {
-        this.put(JobApplication.NAME, name);
-    }
-
-
-
-
     public String getDetail() {
         return getString(JobApplication.DETAIL);
     }
